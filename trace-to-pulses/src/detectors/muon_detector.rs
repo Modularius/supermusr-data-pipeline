a04eb80ad1d27acfd0550ef5e9a288f5916ddac7
Daniel Kirk <daniel.kirk@stfc.ac.uk>
2023-09-05 23:00:23 +0000

    Same commit

--- trace-to-pulses/src/detectors/muon_detector.rs
+++ trace-to-pulses/src/detectors/muon_detector.rs
@@ -8,12 +8,12 @@ use crate::{Detector, Real, RealArray};
 #[derive(Default, Debug, Clone)]
 pub struct MuonData {
     peak : (Real,Real),
-    max_slope : (Real,Real),
-    min_slope : (Real,Real),
+    max_slope : (Real,Real,Real),
+    min_slope : (Real,Real,Real),
     duration : Real,
 }
 impl MuonData {
-    pub fn new(peak : (Real,Real), max_slope : (Real,Real), min_slope : (Real,Real), duration : Real) -> Self {
+    pub fn new(peak : (Real,Real), max_slope : (Real,Real,Real), min_slope : (Real,Real,Real), duration : Real) -> Self {
         MuonData{ peak, max_slope, min_slope, duration }
     }
 }
@@ -21,8 +21,12 @@ impl EventData for MuonData {}
 
 impl Display for MuonData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{0},{1},{2},{3},{4},{5},{6}",
-            self.peak.0,self.peak.1, self.max_slope.0,self.max_slope.1, self.min_slope.0,self.min_slope.1,self.duration))
+        f.write_fmt(format_args!("{0},{1},{2},{3},{4},{5},{6},{7},{8}",
+            self.max_slope.0,self.max_slope.1,self.max_slope.2,
+            self.peak.0,self.peak.1,
+            self.min_slope.0,self.min_slope.1,self.min_slope.2,
+            self.duration
+        ))
     }
 }
 
@@ -44,8 +48,8 @@ pub struct MuonDetector {
     mode : Mode,
     start: Real,
     peak: (Real,Real),
-    max_slope: (Real,Real),
-    min_slope: (Real,Real),
+    max_slope: (Real,Real,Real),
+    min_slope: (Real,Real,Real),
 
     threshold: Real,
     slope_threshold: Real,
@@ -65,25 +69,25 @@ impl Detector for MuonDetector {
             Mode::Flat => {
                 if value.0[0] > self.threshold {
                     self.start = time;
-                    self.max_slope = (time,value.0[1]);
+                    self.max_slope = (time,value.0[0],value.0[1]);
                     self.mode = Mode::Rising;
                 }
                 None
             },
             Mode::Rising => {
                 if self.max_slope.1 < value.0[1] {
-                    self.max_slope = (time,value.0[1]);
+                    self.max_slope = (time,value.0[0],value.0[1]);
                 }
                 if value.0[1] < -self.slope_threshold {
                     self.peak = (time,value.0[0]);
-                    self.min_slope = (time, value.0[1]);
+                    self.min_slope = (time,value.0[0], value.0[1]);
                     self.mode = Mode::Falling;
                 }
                 None
             },
             Mode::Falling => {
                 if self.min_slope.1 > value.0[1] {
-                    self.min_slope = (time,value.0[1]);
+                    self.min_slope = (time,value.0[0],value.0[1]);
                 }
                 if value.0[0] < self.threshold {
                     let event = Some(MuonData::new(self.peak, self.max_slope, self.min_slope,time - self.start).make_event(self.start));
@@ -92,7 +96,7 @@ impl Detector for MuonDetector {
                 } else if value.0[1] > self.slope_threshold {
                     let event = Some(MuonData::new(self.peak, self.max_slope, self.min_slope, time - self.start).make_event(self.start));
                     self.start = time;
-                    self.max_slope = (time,value.0[1]);
+                    self.max_slope = (time,value.0[0],value.0[1]);
                     self.mode = Mode::Rising;
                     event
                 } else {
