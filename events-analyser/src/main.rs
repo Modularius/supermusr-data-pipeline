9519ff4a4fd6704d201a42d943d5ff559f7a3f11
Modularius <daniel.kirk@rocketmail.com>
2024-02-17 00:09:42 +0000

    First Coding

---
+++ events-analyser/src/main.rs
@@ -0,0 +1,126 @@
+mod metrics;
+
+use clap::Parser;
+use kagiyama::{AlwaysReady, Watcher};
+use parameters::Mode;
+use rdkafka::{
+    consumer::{stream_consumer::StreamConsumer, CommitMode, Consumer},
+    message::Message,
+    producer::{FutureProducer, FutureRecord},
+};
+use std::{net::SocketAddr, path::PathBuf, time::Duration};
+use supermusr_streaming_types::dat1_digitizer_analog_trace_v1_generated::{
+    digitizer_analog_trace_message_buffer_has_identifier, root_as_digitizer_analog_trace_message,
+};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(long)]
+    broker: String,
+
+    #[clap(long)]
+    username: Option<String>,
+
+    #[clap(long)]
+    password: Option<String>,
+
+    #[clap(long = "group")]
+    consumer_group: String,
+
+    #[clap(long)]
+    first_topic: String,
+
+    #[clap(long)]
+    second_topic: String,
+
+    #[clap(long, default_value = "127.0.0.1:9090")]
+    observability_address: SocketAddr,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let args = Cli::parse();
+
+    let mut watcher = Watcher::<AlwaysReady>::default();
+    metrics::register(&watcher);
+    watcher.start_server(args.observability_address).await;
+
+    let mut client_config = supermusr_common::generate_kafka_client_config(
+        &args.broker,
+        &args.username,
+        &args.password,
+    );
+
+    let consumer: StreamConsumer = client_config
+        .set("group.id", &args.consumer_group)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "false")
+        .create()
+        .expect("Kafka Consumer should be created");
+
+    consumer
+        .subscribe(&[&args.first_topic, &args.second_topic])
+        .expect("Kafka Consumer should subscribe to given topics");
+
+    let event_pairs = HashMap::new();
+
+    loop {
+        match consumer.recv().await {
+            Ok(m) => {
+                log::debug!(
+                    "key: '{:?}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                    m.key(),
+                    m.topic(),
+                    m.partition(),
+                    m.offset(),
+                    m.timestamp()
+                );
+
+                if let Some(payload) = m.payload() {
+                    if digitizer_event_list_message_buffer_has_identifier(payload) {
+                        metrics::MESSAGES_RECEIVED
+                            .get_or_create(&metrics::MessagesReceivedLabels::new(
+                                metrics::MessageKind::Trace,
+                            ))
+                            .inc();
+                        match root_as_digitizer_event_list_message(payload) {
+                            Ok(thing) => {
+                                let mut event_pair = event_pairs.entry(thing.metadata()).or_insert((None,None))
+                                if m.topic() == args.first_topic {
+                                    *event_pair.0 = (thing.channel(),thing.time(),thing.intensity());
+                                } else if m.topic() == args.second_topic {
+                                    *event_pair.1 = (thing.channel(),thing.time(),thing.intensity());
+                                }
+                                if (Some(event1), Some(event2)) = event_pair {
+                                    event_pairs.remove(thing.metadata())
+                                } 
+                            }
+                            Err(e) => {
+                                log::warn!("Failed to parse message: {}", e);
+                                metrics::FAILURES
+                                    .get_or_create(&metrics::FailureLabels::new(
+                                        metrics::FailureKind::UnableToDecodeMessage,
+                                    ))
+                                    .inc();
+                            }
+                        }
+                    } else {
+                        log::warn!("Unexpected message type on topic \"{}\"", m.topic());
+                        metrics::MESSAGES_RECEIVED
+                            .get_or_create(&metrics::MessagesReceivedLabels::new(
+                                metrics::MessageKind::Unknown,
+                            ))
+                            .inc();
+                    }
+                }
+
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+            Err(e) => log::warn!("Kafka error: {}", e),
+        };
+    }
+}
