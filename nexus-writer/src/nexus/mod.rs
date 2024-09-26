233a19ffaa39421cdf36ecac4d9110efc5a29753
Modularius <daniel.kirk@rocketmail.com>
2024-09-26 08:53:20 +0000

    renamed engine.rs to run_engine.rs

--- nexus-writer/src/nexus/mod.rs
+++ nexus-writer/src/nexus/mod.rs
@@ -1,9 +1,9 @@
-mod engine;
+mod run_engine;
 //mod hdf5_file;
 mod run;
 mod run_parameters;
 
-pub(crate) use engine::{NexusEngine, NexusSettings};
+pub(crate) use run_engine::{NexusEngine, NexusSettings};
 pub(crate) use run::Run;
 pub(crate) use run_parameters::{RunParameters,RunStarted,RunBounded};
 
